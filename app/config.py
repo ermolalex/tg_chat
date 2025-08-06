f2f587c5172f59f3c0645b3976ac95721ea64c2e
ermolalex <alex@kik-soft.ru>
2025-08-06 22:03:19 +0300

    settings without pydantic

--- app/config.py
+++ app/config.py
@@ -3,55 +3,53 @@ from dotenv import load_dotenv
 
 
 class Settings():
-    BOT_TOKEN = ''
-    BASE_SITE = ''
-    ERR = ''
+    BOT_TOKEN: str = ''
+    BASE_SITE: str = ''
+    ADMIN_ID: int = 0
+    ZULIP_API_KEY: str = ''
+    ZULIP_EMAIL: str = ''
+    ZULIP_SITE: str = ''
+    ZULIP_ALLOW_INSECURE: bool = False
+    ZULIP_STAFF_IDS: list = []
 
     def __init__(self, env_file=''):
         load_dotenv()
 
         for attr_name in dir(Settings):
-            if attr_name.isupper():
-                print(attr_name)
-                setattr(Settings, attr_name, self._get_env_val(attr_name))
+            if not attr_name.isupper():
+                continue
+
+            attr_val = getattr(Settings, attr_name)
+            env_val = self._get_env_val(attr_name)
+
+            if isinstance(attr_val, bool):
+                setattr(Settings, attr_name, self._str2bool(env_val))
+            elif isinstance(attr_val, int):
+                setattr(Settings, attr_name, int(env_val))   # если не int - будет ValueError
+            elif isinstance(attr_val, list):
+                setattr(Settings, attr_name, env_val.split(','))
+            else:
+                setattr(Settings, attr_name, env_val)
+
+            # debug
+            # attr_val = getattr(Settings, attr_name)
+            # print(attr_name, ': ', attr_val)
 
     def _get_env_val(self, attr_name):
         val = os.getenv(attr_name)
-        assert val is not None, f'No {attr_name} setting in .env'
+        if val is None:
+            raise NameError(f'No {attr_name} setting in .env')
 
         return val
 
+    def _str2bool(self, str_bool: str):
+        if str_bool.lower() in ('1', 'yes', 'true', 'on'):
+            return True
+        elif str_bool.lower() in ('0', 'no', 'false', 'off'):
+            return False
+        else:
+            raise ValueError(f'Invalid value for BOOL - {str_bool}.')
+
+
 if __name__ == "__main__":
     settings = Settings()
-    print(settings.BOT_TOKEN, settings.ERR)
-
-# Access the string variable
-# my_list_str = os.getenv('MY_LIST')
-
-"""
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-class _Settings(BaseSettings):
-    BOT_TOKEN: str
-    BASE_SITE: str
-    ADMIN_ID: int
-    ZULIP_API_KEY: str
-    ZULIP_EMAIL: str
-    ZULIP_SITE: str
-    ZULIP_ALLOW_INSECURE: bool
-    ZULIP_STAFF_IDS: list[int]
-
-    RABBIT_USER: str
-    RABBIT_USER_PSW: str
-
-    model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
-    )
-
-    def get_webhook_url(self) -> str:
-        # Возвращает URL вебхука с кодированием специальных символов.
-        return f"{self.BASE_SITE}/webhook"
-
-
-settings = Settings()
-"""
